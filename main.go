ba126ff07a28dcca45453a55760f6ed8ad7ef21a
Allandt Bik-Elliott <allandt@gmail.com>
2020-06-10 16:13:16 +0100

    this time create a custom implementation of the Writer interface called logWriter and use that to ouput the Googley frontpage, this time giving the total buffer length

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	res, err := http.Get("https://google.com")
 	if err != nil {
@@ -14,5 +16,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	io.Copy(os.Stdout, res.Body)
+	lw := logWriter{}
+	io.Copy(lw, res.Body)
+}
+
+// implement the Writer interface (https://golang.org/pkg/io/#Writer) to use
+// instead of os.Stdout in our io.Copy() usage
+func (lw logWriter) Write(p []byte) (int, error) {
+	fmt.Println(string(p))
+	fmt.Println("Total bytes processed:", len(p))
+
+	return len(p), nil
 }
